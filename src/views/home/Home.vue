21f73a9224f4c832915f3fa4035517dd43e6f25e
chenyaoze666 <1604275453@qq.com>
2020-04-10 10:13:47 +0800

    TabControl封装和FeatureView封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,6 +5,57 @@
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="titles"></tab-control>
+    <ul>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+    </ul>
   </div>
 </template>
 
@@ -12,6 +63,10 @@
   import NavBar from "components/common/navbar/NavBar";
   import HomeSwiper from "./childComps/HomeSwiper";
   import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
+
+  import TabControl from "components/content/tabControl/TabControl";
+
 
   import {getHomeMultidata} from "network/home";
   
@@ -21,12 +76,15 @@
       NavBar,
       HomeSwiper,
       RecommendView,
+      FeatureView,
+      TabControl,
     },
     data(){
       return{
         //保存数据
         banners: [],
         recommends: [],
+        titles:["流行","新款","精选"],
       }
     },
     created(){
@@ -41,8 +99,25 @@
 </script>
 
 <style scoped>
+  #home{
+    padding-top: 44px;
+  }
+
   .home-nav{
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tab-control{
+    /* 实现固定 */
+    position: sticky;
+    /* 超过指定高度 自动改成 fixed*/
+    top: 44px;
   }
 </style>
