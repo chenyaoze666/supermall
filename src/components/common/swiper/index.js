769f0ac5d39500e1d9900571e6279799deb01be3
chenyaoze666 <1604275453@qq.com>
2020-04-10 09:22:01 +0800

    轮播图封装

---
+++ src/components/common/swiper/index.js
@@ -0,0 +1,6 @@
+import Swiper from './Swiper'
+import SwiperItem from './SwiperItem'
+
+export {//统一导出
+  Swiper, SwiperItem
+}
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,37 @@
+<template>
+  <swiper>
+      <swiper-item v-for="item in banners">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
+</template>
+
+<script>
+// import Swiper from 'components/common/swiper/Swiper';
+  // import SwiperItem from 'components/common/swiper/SwiperItem';
+  //统一导出的 导入方式
+import {Swiper,SwiperItem} from 'components/common/swiper';
+
+
+  export default {
+    name:"HomeSwiper",
+    props:{
+      banners:{
+        type: Array,
+        default(){
+          return []
+        }
+      }
+    },
+    components:{
+      Swiper,
+      SwiperItem,
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
